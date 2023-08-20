d688351d9cc1bcdf46eed7413120fac46c01e1df
aynjel <ortegacanillo76@gmail.com>
2023-08-20 18:18:21 +0800

    adding skills

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,82 +1,113 @@
-import { Card, CardBody, CardHeader,} from 'reactstrap'
+import { Card, CardBody, CardHeader, Row, Col} from 'reactstrap'
 import { Link } from 'react-router-dom'
 
 function Skills() {
     return (
-        <Card className="border-0 text-center">
+        <Card className="border-0 text-center w-100">
             <CardHeader className="bg-transparent border-0">
-                <img src='/images/my-profile-pic.jpg' alt="My Profile" className="img-fluid rounded-circle mb-4" width="200px" style={
-                    {
-                        border: '2px solid #ffc107'
-                    }
-                } />
                 <h1 className="text-warning fw-bold">
-                    Full Stack Developer
+                    Skills
                 </h1>
-                <p className="text-secondary">
-                    Hi, I am <em><strong>Angel Niño Ortega</strong></em>, a passionate and dedicated full stack developer with a degree in BSIT.
-                </p>
-                <ul className="list-unstyled d-flex justify-content-center align-items-center flex-wrap">
-                    <li className="me-3">
-                        <Link to="https://www.facebook.com/" target="_blank">
-                            <img src='/icons/facebook.png' width="30" />
-                        </Link>
-                    </li>
-                    <li className="me-3">
-                        <Link to="https://www.github.com/" target="_blank">
-                            <img src='/icons/code.png' width="30" />
-                        </Link>
-                    </li>
-                    <li className="me-3">
-                        <Link to="https://www.linkedin.com/" target="_blank">
-                            <img src='/icons/linkedin.png' width="30" />
-                        </Link>
-                    </li>
-                    <li className="me-3">
-                        <Link to="https://www.twitter.com/" target="_blank">
-                            <img src='/icons/twitter.png' width="30" />
-                        </Link>
-                    </li>
-                    <li className="me-3">
-                        <Link to="https://www.instagram.com/" target="_blank">
-                            <img src='/icons/instagram.png' width="30" />
-                        </Link>
-                    </li>
-                    <li className="me-3">
-                        <Link to="https://www.youtube.com/" target="_blank">
-                            <img src='/icons/youtube.png' width="30" />
-                        </Link>
-                    </li>
-                    <li className="me-3">
-                        <Link to="https://www.discord.com/" target="_blank">
-                            <img src='/icons/discord.png' width="30" />
-                        </Link>
-                    </li>
-                </ul>
             </CardHeader>
-            <CardBody className='d-flex justify-content-center align-items-center flex-column'>
-                <h3 className="text-warning fw-bold mb-4">
-                    Connect with me
-                </h3>
-                <ul className="list-unstyled d-flex justify-content-center align-items-start flex-column gap-3">
-                    <li className="me-3">
-                        <img src="/icons/gmail.png" width="30" />
-                        <span className="ms-2">
-                            <a href="mailto:ortegacanillo76@gmail.com" className="text-decoration-none text-secondary">
-                                ortegacanillo76@gmail.com
-                            </a>
-                        </span>
-                    </li>
-                    <li className="me-3">
-                        <img src="/icons/telephone.png" width="30" />
-                        <span className="ms-2">
-                            <a href="tel:+639271305088" className="text-decoration-none text-secondary">
-                                +639271305088
-                            </a>
-                        </span>
-                    </li>
-                </ul>
-
+            <CardBody>
+                <Row>
+                    <Col xs={12} md={6} className="mb-4">
+                        <h3 className="text-warning fw-bold mb-4">
+                            Frontend Development
+                        </h3>
+                        <ul className="list-unstyled d-flex justify-content-center align-items-center flex-wrap gap-3">
+                            <li>
+                                <Link to="https://html.com/" target="_blank" rel="noreferrer">
+                                    <img src="/icons/html.png" alt="HTML"  width="40" />
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="https://css-tricks.com/" target="_blank" rel="noreferrer">
+                                    <img src="/icons/css-3.png" alt="CSS"  width="40" />
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="https://www.javascript.com/" target="_blank" rel="noreferrer">
+                                    <img src="/icons/js.png" alt="JavaScript"  width="40" />
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="https://getbootstrap.com/" target="_blank" rel="noreferrer">
+                                    <img src="/icons/bootstrap.png" alt="Bootstrap"  width="40" />
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="https://reactjs.org/" target="_blank" rel="noreferrer">
+                                    <img src="/icons/react.png" alt="React"  width="40" />
+                                </Link>
+                            </li>
+                        </ul>
+                    </Col>
+                    <Col xs={12} md={6} className="mb-4">
+                        <h3 className="text-warning fw-bold mb-4">
+                            Backend Development
+                        </h3>
+                        <ul className="list-unstyled d-flex justify-content-center align-items-center flex-wrap gap-3">
+                            <li>
+                                <Link to="https://nodejs.org/en/" target="_blank" rel="noreferrer">
+                                    <img src="/icons/nodejs.png" alt="Node.js"  width="40" />
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="https://www.mysql.com/" target="_blank" rel="noreferrer">
+                                    <img src="/icons/mysql.png" alt="Mysql"  width="40" />
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="https://www.php.net/" target="_blank" rel="noreferrer">
+                                    <img src="/icons/php.png" alt="PHP"  width="40" />
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="https://cplusplus.com/" target="_blank" rel="noreferrer">
+                                    <img src="/icons/c++.png" alt="C++" width="40" />
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="https://docs.microsoft.com/en-us/dotnet/csharp/" target="_blank" rel="noreferrer">
+                                    <img src="/icons/c-sharp.png" alt="C++" width="40" />
+                                </Link>
+                            </li>
+                        </ul>
+                    </Col>
+                    <Col xs={12} md={12} className="mb-4">
+                        <h3 className="text-warning fw-bold mb-4">
+                            Development Tools and Environments
+                        </h3>
+                        <ul className="list-unstyled d-flex justify-content-center align-items-center flex-wrap gap-3">
+                            <li>
+                                <Link to="https://git-scm.com/" target="_blank" rel="noreferrer">
+                                    <img src="/icons/git.png" alt="Git"  width="40" />
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="https://www.github.com/" target="_blank" rel="noreferrer">
+                                    <img src="/icons/code.png" alt="Github" width="40" />
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="https://www.microsoft.com/en-us/windows/" target="_blank" rel="noreferrer">
+                                    <img src="/icons/windows.png" alt="Windows" width="40" />
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="https://www.office.com/" target="_blank" rel="noreferrer">
+                                    <img src="/icons/office.png" alt="Office" width="40" />
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="https://www.trello.com/" target="_blank" rel="noreferrer">
+                                    <img src="/icons/trello.png" alt="Trello" width="40" />
+                                </Link>
+                            </li>
+                        </ul>
+                    </Col>
+                </Row>
             </CardBody>
         </Card>
     )
